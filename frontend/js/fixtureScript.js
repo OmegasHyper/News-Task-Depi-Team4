e34120d11dcb1d07b3b01cf0a69409556673c280
Mohamed Abdelrazek <killer.omega2005@gmail.com>
2025-09-04 00:27:06 +0300

    adjusted the hierarchy and added fixtures page pseudo structure

---
+++ frontend/js/fixtureScript.js
@@ -0,0 +1,11 @@
+$.ajax({
+    type: 'get',
+    dataType: 'json',
+    url: "http://localhost:5000/fixtures",
+    success: function (data){
+        console.log(data)
+    },
+    error: function (data) {
+        console.log("error")
+    }
+})
