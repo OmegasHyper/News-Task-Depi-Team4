5b77a84f2308b12272886dc5f8f0d67fcd197b02
Mohamed Abdelrazek <killer.omega2005@gmail.com>
2025-09-04 00:21:13 +0300

    added allsportsapi backend for fetching data

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import fetch from "node-fetch";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+var sportsApiKey = '799b803f182d4aea6263113ec7753349199c9307e608accb9303748d1327c5cf';
+var today = new Date("2025-09-03").toISOString().split("T")[0];
+var start = getDate(today, -4).toISOString().split("T")[0];
+var end = getDate(today, 10).toISOString().split("T")[0];
+
+console.log(start)
+
+app.get("/fixtures", async (req, res) => {
+  const url = `https://apiv2.allsportsapi.com/football?met=Fixtures&APIkey=${sportsApiKey}&from=${start}&to=${end}&leagueId=152`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    res.json(data); // send data back to frontend
+  } catch (err) {
+    res.status(500).json({ error: "Failed to fetch data" });
+  }
+});
+
+app.listen(5000, () => console.log("✅ Server running on http://localhost:5000"));
+
+
+function getDate(date, days){
+    const result = new Date(date);
+    result.setDate(result.getDate() + days)
+    return result;
+}
